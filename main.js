0d0b385751717854a7583f6cc99a252397af4f71
Andres Brito <robin_brito@hotmail.com>
2022-09-30 00:27:43 -0500

    version3.0 casi acabada

--- main.js
+++ main.js
@@ -2,7 +2,65 @@ const $ = selector => document.querySelector(selector)
 const display = $('.display')
 const operador = document.querySelectorAll('.operador')
 const numeros = document.querySelectorAll('.number')
+const buttons = document.querySelectorAll('button')
 
+buttons.forEach(numero =>{
+    numero.addEventListener('click',(e)=>{
+        /* const operations = {
+            '/': (a,b)=> a/b,
+            'x': (a,b)=> a*b,
+            '+': (a,b)=> a+b,
+            '-': (a,b)=> a-b,
+            '%': (a)=> a/100
+        }
+        console.log(operations[e.target.innerText](1,2)) */
+        const key = e.target.innerText
+        if(key=='='){
+            display.innerText=roundToTwo(eval(display.innerText))
+        }else if(key == 'x') {
+            display.append('*')
+        }else if(key == 'DEL'){
+            const arr= [...display.innerText] // convierto el contenido del display en un array de caracteres
+            display.innerText = (arr.slice(0,arr.length-1)).join('') // Corto/extraigo todo el array excepto el ultimo caracter y lo convierto en string denuevo
+        }else if(key == 'AC'){
+            display.innerText = ''
+        }else display.append(key)
+    })
+})
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 window.addEventListener('keydown', sePresionoUnaTecla)
 
 function sePresionoUnaTecla(e){
@@ -25,4 +83,4 @@ function operadores(operador) {
     switch(operador){
         case '+':
     }
-}
+} */
